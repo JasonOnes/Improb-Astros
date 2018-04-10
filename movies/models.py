48e4570fd8a7af8ec0d94d988b55bc80d6f9e100
razzlepdx <rwilson.or@gmail.com>
2018-04-09 23:34:57 -0700

    add notes to models.py

--- movies/models.py
+++ movies/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from datetime import date 
-from django.contrib.auth.models import UserManager # django default manager, since no custom user fields
+from datetime import date
+from django.contrib.auth.models import UserManager  # django default manager, since no custom user fields
+
 
 class User(models.Model):
-    name =  models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     email = models.EmailField()
     #maybe a friends many-to-many column
 
@@ -11,19 +12,17 @@ class User(models.Model):
         return self.name
 
 
-
-
 class Movie(models.Model):
     MOVIE_CATEGORIES = (
-    ('C', 'Comedy'),
-    ('D', 'Documentary'),
-    ('H', 'Horror'),
-    ('RC', 'Romantic Comedy'),
-    ('SF', 'Sci-Fi'),
-    ('D', 'Drama'),
-    ('Cl', 'Classic'),
-    ('CC', 'Cult Classic'),
-    )
+        ('C', 'Comedy'),
+        ('D', 'Documentary'),
+        ('H', 'Horror'),
+        ('RC', 'Romantic Comedy'),
+        ('SF', 'Sci-Fi'),
+        ('D', 'Drama'),
+        ('Cl', 'Classic'),
+        ('CC', 'Cult Classic'),
+        )
 
     added = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
@@ -32,8 +31,24 @@ class Movie(models.Model):
     rating = models.PositiveSmallIntegerField(default=0)
     category = models.CharField(choices=MOVIE_CATEGORIES, max_length=2, default='D')
     comments = models.TextField(blank=True, default='')
-    reviewer = models.ForeignKey(User, on_delete=models.CASCADE) #many to one, if user is deleted all movies attributed will also be removed
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE)  # many to one, if user is deleted all movies attributed will also be removed
 
     def __str__(self):
         return self.title
-    
+
+
+class Rating(models.Model):
+    pass
+    # foreign key - user id
+    # foreign key - movie id
+    # int - star rating
+    # textfield - review
+
+
+class MovieList(models.Model):
+    pass
+    # foreign key - user id
+    # foreign key - movie id
+    # charfield - list name
+
+
