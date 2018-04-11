67334458173485a18187793f731fbf6a06761d3a
JasonOnes <jasonr.jones14@gmail.com>
2018-04-10 17:27:23 -0700

    localized

--- improbable_astro/settings.py
+++ improbable_astro/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qq-h9!h3a4%2y3ny$bs)^39eg$@lyu%$3jh!r3ie-yl8c8blzh'
+SECRET_KEY = "SOMETHING-INCREDIBLY-HARD-TO-GUESS"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['0.0.0.0']
-ALLOWED_HOSTS = ['improb-astro.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0']
+#ALLOWED_HOSTS = ['improb-astro.herokuapp.com']
 
 # Application definition
 
@@ -78,23 +78,11 @@ WSGI_APPLICATION = 'improbable_astro.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    #
-    #
-    # 'default': dj_database_url.config()
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dltekb77kf62g',
-        'USER': 'qsnyaoiaobomlj',
-        'PASSWORD': 'cd0fb69617ec03a36f3b4d37f683971cd7ad2d1297e53334e435bff6967cc817',
-        'HOST': 'ec2-54-83-19-244.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    #'default': 
-    #{'URI': 'postgres://qsnyaoiaobomlj:cd0fb69617ec03a36f3b4d37f683971cd7ad2d1297e53334e435bff6967cc817@ec2-54-83-19-244.compute-1.amazonaws.com:5432/dltekb77kf62g'}
+   # here's where the heroku postgres db setups go
 }
 
 
@@ -142,7 +130,5 @@ STATICFILES_DIRS = (
 )
 
 
-# Configure Django App for Heroku.
-#
-django_heroku.settings(locals())
+
 
